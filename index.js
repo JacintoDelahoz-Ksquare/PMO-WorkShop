85eee65b922ec8514b5069663a55710364780d54
MarianaLopez-Ksquare <Mariana.Lopez@itksquare.edu.mx>
2022-11-24 19:56:18 -0600

    code added

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const toggle = document.querySelector('.toggle');
 const links = document.querySelector('.links');
+const sound = new Audio ('./Theme-Song.mp3');
+
+playBtn.addEventListener('click', () =>{
+    sound.play();
+})
 
 toggle.addEventListener ('click', () => {
     toggle.classList.toggle('rotate')
